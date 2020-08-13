63744a8529ab75acb8b672fea65eb564d854f623
Jeff Hughes <jeff.hughes@gmail.com>
2020-08-13 19:17:20 -0400

    Check content type to support non .mp3 files (Fixes #5)

--- src/downloads.rs
+++ src/downloads.rs
@@ -34,21 +34,11 @@ pub struct EpData {
 /// by the user while there are still ongoing jobs.
 pub fn download_list(episodes: Vec<EpData>, dest: &PathBuf, max_retries: usize, threadpool: &Threadpool, tx_to_main: Sender<Message>) {
     // parse episode details and push to queue
-    for mut ep in episodes.into_iter() {
-        let file_name = sanitize_with_options(&ep.title, Options {
-            truncate: true,
-            windows: true,  // for simplicity, we'll just use Windows-friendly paths for everyone
-            replacement: ""
-        });
-
-        let mut file_path = dest.clone();
-        file_path.push(format!("{}.mp3", file_name));
-
-        ep.file_path = Some(file_path);
-
+    for ep in episodes.into_iter() {
         let tx = tx_to_main.clone();
+        let dest2 = dest.clone();
         threadpool.execute(move || {
-            let result = download_file(ep, max_retries);
+            let result = download_file(ep, dest2, max_retries);
             tx.send(Message::Dl(result)).unwrap();
         });
     }
@@ -57,14 +47,8 @@ pub fn download_list(episodes: Vec<EpData>, dest: &PathBuf, max_retries: usize, 
 
 /// Downloads a file to a local filepath, returning DownloadMsg variant
 /// indicating success or failure.
-fn download_file(ep_data: EpData, mut max_retries: usize) -> DownloadMsg {
-    let data = ep_data.clone();
-
-    let dst = File::create(&ep_data.file_path.unwrap());
-    if dst.is_err() {
-        return DownloadMsg::FileCreateError(data);
-    };
-
+fn download_file(ep_data: EpData, dest: PathBuf, mut max_retries: usize) -> DownloadMsg {
+    let mut data = ep_data.clone();
     let request: Result<ureq::Response, ()> = loop {
         let response = ureq::get(&ep_data.url)
             .timeout_connect(5000)
@@ -85,6 +69,33 @@ fn download_file(ep_data: EpData, mut max_retries: usize) -> DownloadMsg {
     };
 
     let response = request.unwrap();
+
+    // figure out the file type
+    let ext = match response.header("content-type") {
+        Some("audio/x-m4a") => "m4a",
+        Some("audio/mpeg") => "mp3",
+        Some("video/quicktime") => "mov",
+        Some("video/mp4") => "mp4",
+        Some("video/x-m4v") => "m4v",
+        _ => "mp3"  // assume .mp3 unless we figure out otherwise
+    };
+    
+    let file_name = sanitize_with_options(&ep_data.title, Options {
+        truncate: true,
+        windows: true,  // for simplicity, we'll just use Windows-friendly paths for everyone
+        replacement: ""
+    });
+    
+    let mut file_path = dest;
+    file_path.push(format!("{}.{}", file_name, ext));
+    
+    let dst = File::create(&file_path);
+    if dst.is_err() {
+        return DownloadMsg::FileCreateError(data);
+    };
+
+    data.file_path = Some(file_path);
+
     let mut reader = response.into_reader();
     return match std::io::copy(&mut reader, &mut dst.unwrap()) {
         Ok(_) => DownloadMsg::Complete(data),
