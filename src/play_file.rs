ce029c5642de75a20822b91a5e10773430398c5a
Jeff Hughes <jeff.hughes@gmail.com>
2022-04-24 14:28:52 -0400

    Feed play command to shell to allow for more complex commands

--- src/play_file.rs
+++ src/play_file.rs
@@ -3,22 +3,17 @@ use std::process::{Command, Stdio};
 
 /// Execute an external shell command to play an episode file and/or URL.
 pub fn execute(command: &str, path: &str) -> Result<()> {
-    // Command expects a command and then optional arguments (giving
-    // everything to it in a string doesn't work), so we need to split
-    // on white space and treat everything after the first word as args
-    let cmd_string = command.to_string();
-    let mut parts = cmd_string.trim().split_whitespace();
-    let base_cmd = parts.next().ok_or_else(|| anyhow!("Invalid command."))?;
-    let mut cmd = Command::new(base_cmd);
-
+    let mut cmd_string = command.to_string();
     if cmd_string.contains("%s") {
         // if command contains "%s", replace the path with that value
-        cmd.args(parts.map(|a| if a == "%s" { path } else { a }));
+        cmd_string = cmd_string.replace("%s", &format!("\"{}\"", path));
     } else {
         // otherwise, add path to the end of the command
-        cmd.args(parts.chain(vec![path].into_iter()));
+        cmd_string = format!("{} \"{}\"", cmd_string, path);
     }
 
+    let mut cmd = Command::new("/bin/sh");
+    cmd.arg("-c").arg(cmd_string);
     cmd.stdout(Stdio::null()).stderr(Stdio::null());
     match cmd.spawn() {
         Ok(_) => Ok(()),
